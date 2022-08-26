7606761e6de440efa11147f709787fedd4a1cea8
irhamrafi <irhamrafimahreza007@gmail.com>
2022-08-26 16:39:03 +0700

    tes doang

---
+++ ArrayCase.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day02
+{
+    internal class ArrayCase
+    {
+        public static void IntroArray()
+        {
+            //declare array with in data Type
+            int[] arr;
+            int[] arrInt = new int[5];
+            arrInt[0] = 12;
+            arrInt[1] = 45;
+            arrInt[3] = 50;
+            arrInt[4] = 1_000_000;
+            //arrINt[6] = 90;
+
+            // int array with string data
+            string[] listOfGirl = new string[] { "yuli", "Rini", "widi" };
+
+            //display Array
+            foreach (var item in listOfGirl)
+            {
+                Console.WriteLine($"{item}");
+            }
+            for (int i = 0; i < listOfGirl.Length; i++)
+            {
+                Console.WriteLine(listOfGirl);
+            }
+        }
+
+        //method return or function inarput array
+
+        public static int[] initArrayInt(int[] arr)
+        {
+            int[] array = arr;
+            return array;
+        }
+
+        public static void DisplayArrayInt(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        //initial value with random
+        public static int[] InitArrayRandom(int n)
+        {
+            int[] arr = new int[n];
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(50);
+            }
+
+            return arr;
+        }
+
+        //Sum All array Elemnent
+        public static double SumAllElement(int[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+
+        public static int FindmaxElement(int[] arr)
+        {
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+using day02;
+
+//AraryCase.IntroArray();
+
+//call function
+int[] n = { 10, 20, 30 };
+var myArray = ArrayCase.initArrayInt(n);
+
+ArrayCase.DisplayArrayInt(myArray);
+WriteLine();
+
+
+
+var myRandom = ArrayCase.InitArrayRandom(20);
+
+ArrayCase.DisplayArrayInt(myRandom);
+Console.WriteLine();
+WriteLine($"Total Sum Random ={ArrayCase.SumAllElement(myRandom)}");
+
+WriteLine($"Nilai Tertinggi = {ArrayCase.FindmaxElement(myRandom)}");
+
+//Copy Array dari myRandom to New aarray
+int[] newArray2 = new int[myRandom.Length];
+for (int i = 0; i < newArray2.Length; i++)
+{
+    newArray2[i] = myRandom[i];
+}
+ArrayCase.DisplayArrayInt(newArray2);
+
+WriteLine();
+
+//sort Array
+Array.Sort(newArray2);
+ArrayCase.DisplayArrayInt(newArray2);
+
+WriteLine();
+
+//Menggunakan Method 
+var randomArray = new int[myRandom.Length];
+Array.Copy(newArray2, 0, randomArray,0, myRandom.Length);
+Array.Reverse(newArray2);
+ArrayCase.DisplayArrayInt(newArray2);
+
+
+
+
+ReadLine();
